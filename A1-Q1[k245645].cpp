f01f7d6095da469e1b00294b7fff5ecfe87d648e
Hashman <hashman@Hashman>
2025-09-17 21:20:34 +0500

    third commit

--- A1-Q1[k245645].cpp
+++ A1-Q1[k245645].cpp
@@ -18,8 +18,23 @@ private:
     long double Bank_Balance;
     
 public:
-     DWS( )  {cout<< "Enter Your Name: ";cin >> Name; cout << "Enter Your IBAN: ";cin >> id;};
+    DWS(){} 
+    DWS(DWS &c )  {c.Name = Name, c.id = id, c.IBAN = IBAN, c.Bank_Balance = Bank_Balance;}; //Deep Copy constructor 
+     //Setters
+     void set_Account(){cout << "Enter Your Name: "; cin >> Name; cout <<"Enter your id: "; cin >> id;}
+     void add_funds(float fund){Bank_Balance += fund;}
      
-    };
+     // Getters
+     double get_check_balance(){return Bank_Balance;}
+     double withdraw_funds(float fund){Bank_Balance -= fund; return Bank_Balance;}
+     string get_name(){return Name;}
+     string get_IBAN(){return IBAN;}};
+// UML DIAGRAM NOT MADE YET
+int main (){
+    DWS obj1; 
+    obj1.set_Account();
+    obj1.add_funds(400.5);
+    obj1.get_check_balance();
+    return 0;}
 
  
