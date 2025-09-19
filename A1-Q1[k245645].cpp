9da7c8d8f7fb39763442622f0134781bdead6f32
Hashman <hashman@Hashman>
2025-09-19 12:55:59 +0500

    class Stocks completed and perfected

--- A1-Q1[k245645].cpp
+++ A1-Q1[k245645].cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 class  DWS //Digital Wallet System
 {
@@ -18,17 +19,37 @@ private:
     long double Bank_Balance = 100;
     
 public:
-    DWS(){} 
-    DWS(DWS &c )  {c.Name = Name, c.id = id, c.IBAN = IBAN, c.Bank_Balance = Bank_Balance;}; //Deep Copy constructor 
+    DWS(){} // default constructor
+    DWS( const DWS  &c )  {Name = c.Name, id = c.id, IBAN = c.IBAN, Bank_Balance = c.Bank_Balance;}; //Deep Copy constructor 
      //Setters
      void set_Account(){cout << "Enter Your Name: "; cin >> Name; cout <<"Enter your id: "; cin >> id;}
      void add_funds(double fund){Bank_Balance += fund;}
-     double withdraw_funds(float fund){Bank_Balance -= fund; return Bank_Balance;}
+     void withdraw_funds(float fund){Bank_Balance -= fund; }
      // Getters
      double get_check_balance(){return Bank_Balance;}
      string get_name(){return Name;}
      string get_IBAN(){return IBAN +id;}};
 // UML DIAGRAM NOT MADE YET
+class stocks{
+    private:
+    int Quantity_owned;
+    public:
+     string ticker_symbol, Company_name;
+    float current_price; 
+    stocks(){cout << "Enter Ticker symbol: "; cin >> ticker_symbol;
+             cout << "Enter Company Name: "; cin >> Company_name;}
+    // deep copy constructor made 
+    stocks(  stocks  &c )  {Company_name = c.Company_name, ticker_symbol = c.ticker_symbol, current_price = c.current_price, Quantity_owned = c.Quantity_owned;}; //Deep Copy constructor 
+   //getter
+   
+   
+   double holding_value(){ 
+        cout << "Enter current price: "; cin >> current_price;
+        cout << "Enter Quantity Owned: "; cin >> Quantity_owned;
+        return {Quantity_owned * current_price};}
+
+
+};
 int main (){
     DWS obj1; 
     obj1.set_Account();
@@ -36,6 +57,22 @@ int main (){
     cout << obj1.get_IBAN()<< endl;
      obj1.add_funds(400.5);
     cout << obj1.get_check_balance() <<" is your Bank Balance" << endl;
-    return 0;}
+    cout << "Now the Deep Copy Constructor will be called" << endl;
+   DWS obj2(obj1); // Copy constructor called
+    cout << obj2.get_name() <<endl;
+    cout << obj2.get_IBAN()<< endl;
+    cout << obj2.get_check_balance() <<" is your Bank Balance" << endl;
+    
+
+    stocks client1; //Object of stock portfolio
+    cout << "The holding value is \n" << client1.holding_value() <<"Rs" << endl;
+    // copy constructor called
+    cout << "copy constructor called "<< endl;
+    stocks client2(client1);
+    cout << "The holding value is \n" << client2.holding_value() <<"Rs" << endl;
+    
+
+        return 0;}
+
 
  
